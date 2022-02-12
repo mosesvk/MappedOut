89455d022d8f12d67ad84099042202adf61baccc
mosesvk <kaumatule93@gmail.com>
2022-02-12 06:54:37 -0700

    working on Mongoose connect

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,36 @@
-import express from 'express'
-import bodyParser from 'body-parser'
+import express from 'express';
+import bodyParser from 'body-parser';
+import colors from 'colors';
+import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 
-import placesRoutes from './routes/places_routes.js'
-import userRoutes from './routes/users_routes.js'
+import placesRoutes from './routes/places_routes.js';
+import userRoutes from './routes/users_routes.js';
 import { errorHandler, HttpError } from './models/errorHandler.js';
 
 const app = express();
 
-app.use(bodyParser.json())
+connectDB();
 
-app.use('/api/places', placesRoutes)
-app.use('/api/users', userRoutes)
+const PORT = process.env.PORT
+
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes);
+app.use('/api/users', userRoutes);
 
 // error no route found
 app.use((req, res, next) => {
   const error = new HttpError('Could not find this route', 404);
-  throw error
-})
+  throw error;
+});
 
 //error middleware function
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(5000)
+app.listen(
+  5000,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
