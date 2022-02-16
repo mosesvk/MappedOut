ed26c1681993e04e025d00668e7a4525b873a0d4
mosesvk <kaumatule93@gmail.com>
2022-02-15 19:00:38 -0700

    Creating the User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
+});
+
+userSchema.plugin(uniqueValidator);
+// making sure that we only create a new user if the email was NOT used already
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
