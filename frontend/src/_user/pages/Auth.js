ce314533bc148d27a169808bd997e5ea47c76e1f
mosesvk <kaumatule93@gmail.com>
2022-02-22 14:16:23 -0700

    updating auth process

--- frontend/src/_user/pages/Auth.js
+++ frontend/src/_user/pages/Auth.js
@@ -3,8 +3,6 @@ import React, { useState, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import ErrorModal from '../../shared/components/UIElements/ErrorModal'
-import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
