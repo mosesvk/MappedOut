9b2ee036735954d174bedc9a07c5241e99d69a5d
mosesvk <kaumatule93@gmail.com>
2022-02-23 16:16:54 -0700

    updating file backend

--- backend/routes/users_routes.js
+++ backend/routes/users_routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { check } from 'express-validator';
 import { getAllUsers, login, signup } from '../controllers/users_controller.js';
+import fileUpload from '../middleware/file-upload'
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.post(
   '/signup',
+  fileUpload.single('image'),
   [
     check('name').trim().notEmpty(),
     check('email').normalizeEmail().isEmail(),
